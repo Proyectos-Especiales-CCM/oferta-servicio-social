dac8a2f3a779517e11b1b449c97b364b3d4e6e1e
Diego Jacobo Martínez <diegojmr5@gmail.com>
2025-05-17 18:15:37 -0600

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,14 +7,14 @@ import { ProjectsContextProvider } from '@/context/ProjectsContext';
 
 
 export const metadata: Metadata = {
-  title: 'Oferta Servicio Social - Febrero - Junio - 2025',
-  description: 'Oferta de proyectos de Servicio Social Tec CCM para el periodo de Febrero - Junio 2025',
+  title: 'Oferta Servicio Social - Verano y Agosto Diciembre 2025',
+  description: 'Oferta de proyectos de Servicio Social Tec CCM para el periodo de Verano y Agosto Diciembre 2025',
   metadataBase: new URL('https://oferta-servicio-social.vercel.app/'),
   openGraph: {
     type: 'website',
     url: 'https://oferta-servicio-social.vercel.app/',
-    title: 'Oferta Servicio Social - Febrero - Junio - 2025',
-    description: 'Oferta de proyectos de Servicio Social Tec CCM para el periodo de Febrero - Junio 2025',
+    title: 'Oferta Servicio Social - Verano y Agosto Diciembre 2025',
+    description: 'Oferta de proyectos de Servicio Social Tec CCM para el periodo de Verano y Agosto Diciembre 2025',
     images: [
       {
         url: 'https://oferta-servicio-social.vercel.app/opengraph/opengraph-image.png',
